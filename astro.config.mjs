617f2dde852e35ba15ca2778c3e9881ae9f37c87
Mark Morgan <65377051+markmo123@users.noreply.github.com>
2025-06-15 15:37:29 +0800

    added post-1 and rebased

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://markmo123.github.io',
-    base: 'astrotest',
+  site: 'https://markmo123.github.io/astrotest/',
 });
