6ec5fa82d65f29fd9d0486d9e0027a781a773d33
Mark Morgan <65377051+markmo123@users.noreply.github.com>
2025-06-15 20:17:52 +0800

    actually fix base and add frontmatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://markmo123.github.io/astrotest/',
+  site: 'https://markmo123.github.io',
+  base: 'astrotest',
 });
