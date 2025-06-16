31323ba391d89be4b3015931ef8be79213c456d3
Mark Morgan <65377051+markmo123@users.noreply.github.com>
2025-06-16 22:49:21 +0800

    add greeting and light/dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://markmo123.github.io',
   base: 'astrotest',
+  integrations: [preact()],
 });
